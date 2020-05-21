f2ff341f7f0880e393af5a042ddf275ab34297dc
pd <745282469@qq.com>
2020-05-21 16:29:53 +0800

    Initial commit

---
+++ app/src/main/java/com/dong/studygamesdk/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.dong.studygamesdk
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ mysdk/src/main/java/com/dong/mysdk/LoginManager.kt
@@ -0,0 +1,15 @@
+package com.dong.mysdk
+
+import android.os.Bundle
+import com.alipay.sdk.app.OpenAuthTask
+
+/**
+ * Create by AndroidStudio
+ * Author: pd
+ * Time: 2020/5/21 15:47
+ */
+class LoginManager {
+    private val zfbCallback = OpenAuthTask.Callback { p0, p1, p2 ->
+
+    }
+}
